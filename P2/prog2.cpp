4af2f8262ae895d7d2e7b3a0069b7cb64913b2fa
Anna Chen <106anna@gmail.com>
2021-04-21 02:35:03 -0700

    part 1 matches project description sample output

--- P2/prog2.cpp
+++ P2/prog2.cpp
@@ -1,41 +1,44 @@
 #include "prog2.hpp"
 #include <cmath>
 
-std::tuple<bool, std::vector<unsigned>> isSubsetSum(const std::vector<int>& itemVals, int target) {
-    std::vector<unsigned> sol; // push_back same as append
-    std::tuple<bool, std::vector<unsigned>> res(false, sol);
-    // MUST INVOLVED BITS
-    // no more than 16 items -> hexadecimal!
+#include <iostream> // delete later
 
-    for (int i = 0; i < pow(2, itemVals.size()) - 1; i++) { // iterate over all possible subsets of the (max) 16 items
-        // turn each i into a different bitstring
-        int temp = 1;
-        int tempSum = 0;
-        std::vector<unsigned> tempInd;
-        for (int j = 0; j < 16; j++) {
-            int temp2 = i & temp;
-            if (temp2 != 0) {
-                int bitNum = log2(temp2);
-                int index = itemVals.size() - bitNum - 1;
-                tempInd.push_back(index);
-                tempSum += itemVals[index];
+std::tuple<bool, std::vector<unsigned>> isSubsetSum(const std::vector<int>& itemVals, int target) {
+    std::vector<unsigned> indices; // indices of values that make up the optimal solution
+    std::tuple<bool, std::vector<unsigned>> res(false, indices);
+
+    // iterate over all possible subsets of the (max) 16 items -> 2^n possibilities
+    for (int count = 0; count < pow(2, itemVals.size()); count++) {
+        int sum = 0;
+        indices.clear();
+
+        // find sum of subset
+        // to add indices in ascending order, start with MSB
+        for (int mask = pow(2, 15); mask > 0; mask >>= 1) {
+            int temp = count & mask; // determine which bit to add
+            if (temp != 0) {
+                int bitNum = log2(temp); // determine which bit number it is (diff from index)
+                int i = itemVals.size() - bitNum - 1; // compute index 
+                indices.push_back(i);
+                sum += itemVals[i];
             }
-            temp = temp << 1;
         }
-        if (tempSum == target) {
+
+        // if sum == target, update tuple and break (at most 1 optimal solution)
+        if (sum == target) {
             std::get<bool>(res) = true;
-            std::get<std::vector<unsigned>>(res) = tempInd;
+            std::get<std::vector<unsigned>>(res) = indices;
             break;
         }
-        // position of 1's in bitstring correspond to adding respective element in itemVals for temporary sum
-        // if that sum is equal to target, optimal solution is found
-            // change tuple and break loop
     }
-    return res; // return false, empty vector (no optimal solution)
+    return res;
 }
 
 std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> isSetPartionable(const std::vector<int>& vals) {
     // TODO
-    std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> res;
+    std::vector<unsigned> par1, par2;
+    std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> res(false, par1, par2);
+
+
     return res;
 }
