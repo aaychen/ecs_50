629def363422b9ea45d554e321d20868875f7e48
Anna Chen <106anna@gmail.com>
2021-04-21 13:59:39 -0700

    cleaned up code

--- P2/prog2.cpp
+++ P2/prog2.cpp
@@ -3,13 +3,12 @@
 
 std::tuple<bool, std::vector<unsigned>> isSubsetSum(const std::vector<int>& itemVals, int target) {
     std::vector<unsigned> indices; // indices of values that make up the optimal solution
-    std::tuple<bool, std::vector<unsigned>> res(false, indices);
-
+    
     // iterate over all possible subsets of the (max) 16 items -> 2^n possibilities
     for (int count = 0; count < pow(2, itemVals.size()); count++) {
         int sum = 0;
         indices.clear();
-
+        
         // find sum of subset
         // to add indices in ascending order, start with MSB
         for (int mask = pow(2, itemVals.size()-1); mask > 0; mask >>= 1) {
@@ -21,22 +20,21 @@ std::tuple<bool, std::vector<unsigned>> isSubsetSum(const std::vector<int>& item
                 sum += itemVals[i];
             }
         }
-
-        // if solution set is not empty and sum == target, update tuple and break (at most 1 optimal solution)
-        if (indices.size() != 0 && sum == target) {
-            std::get<bool>(res) = true;
-            std::get<std::vector<unsigned>>(res) = indices;
-            break;
+        
+        // if solution set is not empty and sum == target, create true tuple and return (at most 1 optimal solution)
+        if (!indices.empty() && sum == target) {
+            return std::tuple<bool, std::vector<unsigned>>(true, indices);
         }
     }
-    return res;
+
+    // no optimal solution found
+    return std::tuple<bool, std::vector<unsigned>>(false, std::vector<unsigned>());
 }
 
 std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> isSetPartionable(const std::vector<int>& vals) {
-    // TODO
     std::vector<unsigned> par1, par2; // indices of values that make up the optimal solution
-    std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> res(false, par1, par2);
 
+    // iterate over all possibilities
     for (int count = 0; count < pow(2, vals.size()); count++) {
         int sum1 = 0, sum2 = 0;
         par1.clear();
@@ -44,7 +42,7 @@ std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> isSetPartionable(
 
         // find sums of partitions
         // to add indices in ascending order, start with MSB
-        int j = 0; // keep track of index position for adding to par2
+        int j = 0; // keep track of index position for adding to par1
         for (int mask = pow(2, vals.size()-1); mask > 0; mask >>= 1) {
             int temp = count & mask; // determine which bit to add to which partition
             if (temp != 0) { // add to par2 (par2 first to match output -- no other reason)
@@ -59,13 +57,12 @@ std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> isSetPartionable(
             j++;
         }
 
-        // if partitions are not empty and sum1 == sum2, update tuple and break (at most 1 optimal solution)
-        if (par1.size() != 0 && par2.size() != 0 && sum1 == sum2) {
-            std::get<bool>(res) = true;
-            std::get<1>(res) = par1;
-            std::get<2>(res) = par2;
-            break;
+        // if partitions are not empty and sum1 == sum2, create true tuple and return (at most 1 optimal solution)
+        if (!par1.empty() && !par2.empty() && sum1 == sum2) {
+            return std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>>(true, par1, par2);
         }
     }
-    return res;
+
+    // no optimal solution found
+    return std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>>(false, std::vector<unsigned>(), std::vector<unsigned>());
 }
