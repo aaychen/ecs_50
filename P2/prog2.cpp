266970cd1f44b81c59785ec7199dd5a1ccdb0fed
Anna Chen <106anna@gmail.com>
2021-04-21 03:28:53 -0700

    deleted commented out debugging section

--- P2/prog2.cpp
+++ P2/prog2.cpp
@@ -1,8 +1,6 @@
 #include "prog2.hpp"
 #include <cmath>
 
-#include <iostream> // delete later
-
 std::tuple<bool, std::vector<unsigned>> isSubsetSum(const std::vector<int>& itemVals, int target) {
     std::vector<unsigned> indices; // indices of values that make up the optimal solution
     std::tuple<bool, std::vector<unsigned>> res(false, indices);
@@ -14,9 +12,7 @@ std::tuple<bool, std::vector<unsigned>> isSubsetSum(const std::vector<int>& item
 
         // find sum of subset
         // to add indices in ascending order, start with MSB
-
-        // change 15 to itemVals.size() ???
-        for (int mask = pow(2, 15); mask > 0; mask >>= 1) {
+        for (int mask = pow(2, itemVals.size()-1); mask > 0; mask >>= 1) {
             int temp = count & mask; // determine which bit to add
             if (temp != 0) {
                 int bitNum = log2(temp); // determine which bit number it is (diff from index)
@@ -62,17 +58,6 @@ std::tuple<bool, std::vector<unsigned>, std::vector<unsigned>> isSetPartionable(
             }
             j++;
         }
-        
-        // debugging
-        // for (auto num : par1) {
-        //     std::cout << num << ' ';
-        // }
-        // std::cout << std::endl;
-        // for (auto num : par2) {
-        //     std::cout << num << ' ';
-        // }
-        // std::cout << std::endl;
-        // std::cout << std::endl;
 
         // if partitions are not empty and sum1 == sum2, update tuple and break (at most 1 optimal solution)
         if (par1.size() != 0 && par2.size() != 0 && sum1 == sum2) {
