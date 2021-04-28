072d0b38a628ad32176a8ce62788c5091a17b7ed
Anna Chen <106anna@gmail.com>
2021-04-28 15:58:11 -0700

    pt1-2 run correctly on sample input

--- P3/Provided_Code/prod_arr.s
+++ P3/Provided_Code/prod_arr.s
@@ -9,6 +9,17 @@
 .globl _start
 _start:
     # TODO: Your code goes here.
+    mov $5, %eax # counter
+    mov $1, %ebx # product
+    mov $arr, %ecx
+
+loop:
+    imul (%ecx), %ebx # multiplication
+    add $4, %ecx # increment pointer
+    dec %eax # decrement counter
+    jnz loop
+    mov %ebx, prod
+
 
 done:
     nop
--- P3/Provided_Code/sum_other.s
+++ P3/Provided_Code/sum_other.s
@@ -9,6 +9,25 @@
 .globl _start
 _start:
     # TODO: Your code goes here.
+    mov $0, %r8d # counter
+    mov $0, %r9d # sum
+    mov $2, %r10d
+    mov $0, %r11d # index
+    
+loop:
+    mov %r8d, %eax
+    mov $0, %edx
+    idiv %r10d # quotient in EAX, remainder in EDX
+    cmp $0, %edx # check remainder
+    jne incrementCounter
+    add arr(%r11d), %r9d
+    
+incrementCounter:
+    inc %r8d
+    add $4, %r11d
+    cmp arrlen, %r8d
+    jl loop
+    mov %r9d, sum
 
 done:
     nop
