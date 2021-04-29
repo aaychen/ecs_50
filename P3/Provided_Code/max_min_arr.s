90463bd7cec30226278aee2b9dd49a138c5de3d4
Anna Chen <106anna@gmail.com>
2021-04-29 01:42:28 -0700

    fixed comparing index to value bug; added registers to hold max/min values

--- P3/Provided_Code/max_min_arr.s
+++ P3/Provided_Code/max_min_arr.s
@@ -8,29 +8,33 @@ _start:
     mov arrlen, %r8d
     imul $4, %r8d
     mov $0, %r9d # index
-    mov $0, %r10d # minIndex
-    mov $0, %r11d # maxIndex
+    mov $0, %r10d # min index
+    mov $0, %r11d # max index
     mov $4, %r12d
+    mov arr(%r9d), %r13d # min value
+    mov %r13d, %r14d # max value
 
 loop:
-    cmp %r10d, arr(%r9d)
+    cmp %r13d, arr(%r9d) # check min
     jl updateMin
 
 checkMax:
-    cmp %r10d, arr(%r9d)
+    cmp %r14d, arr(%r9d)
     jg updateMax
     jmp incrementIndex
 
 updateMin:
     mov %r9d, %eax
     idiv %r12d
-    mov %eax, %r10d
+    mov %eax, %r10d # update min index
+    mov arr(%r9d), %r13d # update min value
     jmp checkMax
 
 updateMax:
     mov %r9d, %eax
     idiv %r12d
-    mov %eax, %r11d
+    mov %eax, %r11d # update max index
+    mov arr(%r9d), %r14d # update max value
 
 incrementIndex:
     add $4, %r9d
