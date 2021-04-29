ded9cdac8be2202eb413a29c4c95b1a3f7ac3b43
Anna Chen <106anna@gmail.com>
2021-04-28 18:11:28 -0700

    works on sample input

--- P3/Provided_Code/count_target.s
+++ P3/Provided_Code/count_target.s
@@ -2,13 +2,24 @@
 
 .include "input_count_target.s"
 
-# TODO: Any additional variables that you need go here.
-
-
 .text
 .globl _start
 _start:
-    # TODO: Your code goes here.
+    mov $0, %r8d # count
+    mov $0, %r9d # index
+    mov $len, %r10d # string length
+
+loop:
+    mov str(%r9d), %r11b # char at str[i]
+    cmp target, %r11b 
+    jne incrementIndex
+    inc %r8d # increment count
+
+incrementIndex:
+    inc %r9d
+    cmp %r10d, %r9d # check i < len
+    jl loop
+    mov %r8d, count
 
 done:
     nop
