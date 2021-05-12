685a63c7331c035c2753b2ac8fe1f4797ef7afb6
Anna Chen <106anna@gmail.com>
2021-05-11 18:05:39 -0700

    part4 changed to handle 64 bits for arg1

--- P4/check_bits.s
+++ P4/check_bits.s
@@ -2,39 +2,25 @@
 
 checkBits:
 # referred to https://flint.cs.yale.edu/cs421/papers/x86-asm/asm.html for shl usage
-# Assume working with 32 bit integers
-# Arguments passed on stack in order (push num, a, b)
+# Arguments pushed on stack in order: num, a, b
+# Assume 0 <= a,b <= 31
 # Place return value in RAX
-
-# num = integer_to_check
-# a = bit number (0 -> LSB), 0 <= a <= 31
-# b = bit number, 0 <= b <= 31
-# To check if 1 is set in bit number a, b:
-#     find 2^a and 2^b (can use left shift to multiply by 2)
-#     bool1 = bitwise AND operation on 2^a and num
-#     bool2 = bitwise AND operation on 2^b and num
-#     #if bool1 and bool2:
-#         return 1
-#     #elif:
-#         return 0
-    movq 24(%rsp), %r8 # first argument -> value
-    movb 16(%rsp), %r9b # second argument -> first bit number, a
-    movb 8(%rsp), %r10b # third argument -> second bit number, b
+    movq 24(%rsp), %r8 # 1st arg: num
+    movb 16(%rsp), %r9b # 2nd arg: first bit number, a
+    movb 8(%rsp), %r10b # 3rd arg: second bit number, b
 checkBitA:
-    movq $1, %r11 # create mask; upper 32 bits should be 0)
+    movq $1, %r11 # create mask
     movb %r9b, %cl
-    shl %cl, %r11d # left shift mask a times
-    mov %r8d, %r12d
-    and %r12d, %r11d # bitwise AND to see if bit a is set to 1
-    cmp $0, %r11d
+    shl %cl, %r11 # left shift mask a times
+    and %r8, %r11 # bitwise AND to see if bit a is set to 1
+    cmp $0, %r11
     je false
 checkBitB:
     movq $1, %r11 # create mask
     movb %r10b, %cl
-    shl %cl, %r11d # left shift mask b times
-    mov %r8d, %r12d
-    and %r12d, %r11d # bitwise AND to see if bit b is set to 1
-    cmp $0, %r11d
+    shl %cl, %r11 # left shift mask b times
+    and %r8, %r11 # bitwise AND to see if bit b is set to 1
+    cmp $0, %r11
     je false
 true:
     mov $1, %rax
