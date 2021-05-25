b8604d38ea725e2828db35c140dc375486a61e94
Anna Chen <106anna@gmail.com>
2021-05-24 23:43:04 -0700

    modified to print out output matrix in row major order

--- P5/call_transpose.s
+++ P5/call_transpose.s
@@ -13,6 +13,8 @@ output:
     .word -1
     .endr
 
+format_str: .string "output[%d]: %d\n"
+
 .text
 .globl main
 main:
@@ -23,6 +25,27 @@ main:
     lui a3, %hi(output)
     addi a3, a3, %lo(output)
     jal transpose
+
+    # Print output matrix items in row major order
+    li s0, 0 # loop variable i
+    mul s1, a1, a2 # numItems = numRows * numCols
+    # Load format string (starting address) into s3
+    lui s3, %hi(format_str)
+    addi s3, s3, %lo(format_str)
+    mv s4, a3
+printLoop:
+    bge s0, s1, afterLoop
+    # Set up arguments to printf
+    mv a0, s3 # a0 = s3 (format string)
+    mv a1, s0 # a1 = s0 (loop variable)
+    slli t0, a1, 2 # t0 = a1 << 2
+    add t0, t0, s4 # t0 = t0 + output
+    lw a2, 0(t0) # a2 = Mem[0 + t0]
+    call printf
+    addiw s0, s0, 1
+    j printLoop
+
+afterLoop:
     # Perform exit() system call.
     li a7, 93
     li a0, 0
