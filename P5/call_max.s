9c41e38c00c873272cd2e749597df1e60ec202e2
Anna Chen <106anna@gmail.com>
2021-05-26 21:12:07 -0700

    added test cases

--- P5/call_max.s
+++ P5/call_max.s
@@ -5,6 +5,7 @@ format_str: .string "Max: %d\n"
 .text
 .globl main
 main:
+    # Test 1
     li a0, 5
     li a1, 13
     jal max
@@ -12,6 +13,70 @@ main:
     lui a0, %hi(format_str)
     addi a0, a0, %lo(format_str)
     call printf
+
+    # Test 2
+    li a0, 20
+    li a1, 13
+    jal max
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf
+
+    # Test 3
+    li a0, 100
+    li a1, 100
+    jal max
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf
+
+    # Test 4
+    li a0, -23
+    li a1, -10
+    jal max
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf
+
+    # Test 5
+    li a0, -15
+    li a1, -40
+    jal max
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf
+
+    # Test 6
+    li a0, -294
+    li a1, -294
+    jal max
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf
+
+    # Test 7
+    li a0, -23
+    li a1, 49
+    jal max
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf
+
+    # Test 8
+    li a0, 239
+    li a1, -40
+    jal max
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf
+
     # Perform exit() system call.
     # Source: https://www.robalni.org/riscv/linux-syscalls-64.html
     li a7, 93
--- P5/call_sum_other.s
+++ P5/call_sum_other.s
@@ -2,6 +2,42 @@
 
 format_str: .string "Sum: %d\n"
 
+arr2:
+    .long 42
+
+arr3:
+    .long 14
+    .long 139
+
+arr4:
+    .long 5
+    .long 20
+    .long 113
+    .long 5108
+    .long 27
+    .long 42
+    .long 23874
+
+arr5:
+    .long 5
+    .long 20
+    .long 13
+    .long 18
+    .long 27
+    .long 42
+    .long 3
+    .long 135
+    .long 51
+    .long 2
+
+arr6:
+    .long -5
+    .long -20
+    .long -13
+    .long -18
+    .long -27
+    .long -42
+
 arr:
     .long 5
     .long 20
@@ -13,14 +49,76 @@ arr:
 .text
 .globl main
 main:
-    lui a0, %hi(arr)
-    addi a0, a0, %lo(arr)
-    li a1, 6
+    # Test 1: empty array (pass arrlen = 0)
+    lui a0, %hi(arr2)
+    addi a0, a0, %lo(arr2)
+    li a1, 0 # arrlen
     jal sum_other
     mv a1, a0
     lui a0, %hi(format_str)
     addi a0, a0, %lo(format_str)
-    call printf
+    call printf # 0
+
+    # Test 2
+    lui a0, %hi(arr2)
+    addi a0, a0, %lo(arr2)
+    li a1, 1 # arrlen
+    jal sum_other
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf # 42
+
+    # Test 3
+    lui a0, %hi(arr3)
+    addi a0, a0, %lo(arr3)
+    li a1, 2 # arrlen
+    jal sum_other
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf # 14
+
+    # Test 4
+    lui a0, %hi(arr4)
+    addi a0, a0, %lo(arr4)
+    li a1, 7 # arrlen
+    jal sum_other
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf # 24019
+
+    # Test 5
+    lui a0, %hi(arr5)
+    addi a0, a0, %lo(arr5)
+    li a1, 10 # arrlen
+    jal sum_other
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf # 99
+
+    # Test 6
+    lui a0, %hi(arr6)
+    addi a0, a0, %lo(arr6)
+    li a1, 6 # arrlen
+    jal sum_other
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf # -45
+
+    # Sample test from prompt
+    lui a0, %hi(arr)
+    addi a0, a0, %lo(arr)
+    li a1, 6 # arrlen
+    jal sum_other
+    mv a1, a0
+    lui a0, %hi(format_str)
+    addi a0, a0, %lo(format_str)
+    call printf # 45
+
     # Perform exit() system call.
     li a7, 93
     li a0, 0
