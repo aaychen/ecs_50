9d01683d6d15387648aed547cd20e8288b47978b
Anna Chen <106anna@gmail.com>
2021-06-08 12:31:19 -0700

    screen design done, border collision checking done

--- P6/src/main.c
+++ P6/src/main.c
@@ -1,5 +1,10 @@
 #include <stdint.h>
 
+// 1) Open xquartz
+// 2) ssh -X
+// 3) export PATH=$PATH:/home/aaron123/opt/riscv32/bin 
+// 4) /home/aaron123/riscv-console/runsim.sh
+
 volatile int global = 42;
 volatile uint32_t controller_status = 0;
 
@@ -8,46 +13,50 @@ int main() {
     int a = 4;
     int b = 12;
     int last_global = 42;
-    int x_pos = 12;
+    int x_pos;
 
-    VIDEO_MEMORY[0] = 'H';
-    VIDEO_MEMORY[1] = 'e';
-    VIDEO_MEMORY[2] = 'l';
-    VIDEO_MEMORY[3] = 'l';
-    VIDEO_MEMORY[4] = 'o';
-    VIDEO_MEMORY[5] = ' ';
-    VIDEO_MEMORY[6] = 'W';
-    VIDEO_MEMORY[7] = 'o';
-    VIDEO_MEMORY[8] = 'r';
-    VIDEO_MEMORY[9] = 'l';
-    VIDEO_MEMORY[10] = 'd';
-    VIDEO_MEMORY[11] = '!';
-    VIDEO_MEMORY[12] = 'X';
-
+    // screen = 64 columns, 36 rows
+    int numCols = 64, numRows = 36;
+    for (int col = 0; col < numCols; col++) {
+        for (int row = 0; row < numRows; row++) {
+            if (row == 1 && col == 1) { // top-left point for user starting point of X
+                VIDEO_MEMORY[col + row * numCols] = 'X';
+                x_pos = col + row * numCols;
+            } else if (row == 0 || row == numRows-1) {  // top or bottom row
+                VIDEO_MEMORY[col + row * numCols] = '=';
+            } else if (col == 0 || col == numCols-1) { // left or right column
+                VIDEO_MEMORY[col + row * numCols] = '|';
+            } else {
+                VIDEO_MEMORY[col + row * numCols] = '.';
+            }
+        }
+    }
 
     while (1) {
         int c = a + b + global;
         if(global != last_global){
             if(controller_status){
                 VIDEO_MEMORY[x_pos] = ' ';
-                if(controller_status & 0x1){
-                    if(x_pos & 0x3F){
-                        x_pos--;
+                if(controller_status & 0x10){ // check bit 4 of controller status (B1D, default: u)
+                    if((x_pos & 0x3F) != 0x1) { // check left '|' border; if 0x1, then user in second column
+                        x_pos--; // move left
                     }
                 }
-                if(controller_status & 0x2){
-                    if(x_pos >= 0x40){
-                        x_pos -= 0x40;
+                if(controller_status & 0x20){ // check bit 5 of controller status (B2D, default: i)
+                    // if(x_pos >= 0x40){ // check top border; 0x40 = 64_10 starts second row
+                    if(x_pos >= 0x40 + 0x40){ // check top '=' border; 0x40 + 0x40 starts third row
+                        x_pos -= 0x40; // move up
                     }
                 }
-                if(controller_status & 0x4){
-                    if(x_pos < 0x8C0){
-                        x_pos += 0x40;
+                if(controller_status & 0x40){ // check bit 6 of controller status (B3D, default: j)
+                    // if(x_pos < 0x8C0){ // check bottom border; 0x8C0 = 2240_10 = 35*64 starts last row
+                    if(x_pos < 0x8C0 - 0x40){ // check bottom '=' border; 0x8C0 - 0x40 starts second-last row
+                        x_pos += 0x40; // move down
                     }
                 }
-                if(controller_status & 0x8){
-                    if((x_pos & 0x3F) != 0x3F){
-                        x_pos++;
+                if(controller_status & 0x80){ // check bit 7 of controller status (B4D, default: k)
+                    if((x_pos & 0x3F) != (0x3F - 0x1)){ // check right '|' border; if 0x3F - 0x1, then user in second-last column
+                        x_pos++; // move right
                     }
                 }
                 VIDEO_MEMORY[x_pos] = 'X';
